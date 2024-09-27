8ca9a6b4603759434349dd36a4f61cf903c97db4
adityatambi <adityatambi10@gmail.com>
2024-09-27 17:15:54 +0530

    post new form data using fetch

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
-import { PostData } from './types';
+import { NewPostData, PostData } from './types';
 import { getPosts } from './getPosts';
 import { PostsList } from './PostsList';
+import { NewPostForm } from './NewPostForm';
+import { savePost } from './savePost';
 
 export function PostsPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,6 +21,11 @@ export function PostsPage() {
     };
   }, []);
 
+  async function handleSave(NewPostData: NewPostData) {
+    const newPost = await savePost(NewPostData);
+    setPosts([newPost, ...posts]);
+  }
+
   if (isLoading) {
     return <div className="w-96 mx-auto mt-6">Loading...</div>;
   }
@@ -26,6 +33,7 @@ export function PostsPage() {
   return (
     <div className="w-96 mx-auto mt-6">
       <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
+      <NewPostForm onSave={handleSave} />
       <PostsList posts={posts} />
     </div>
   );
